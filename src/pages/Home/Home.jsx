d003048a6ea4126a985167e6cba32989ecb39ef5
Your Name <naimur202163@gmail.com>
2022-08-22 14:42:54 +0600

    navbar added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import TableData from '../TableData/TableData'
+import React from "react";
+import Navbars from "../Shared/Navbar/Navbar";
+import TableData from "../TableData/TableData";
 
 export default function Home() {
   return (
-    <div><TableData/></div>
-  )
+    <div>
+      <Navbars />
+      <TableData />
+    </div>
+  );
 }
